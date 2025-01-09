b07970653ab328d775c7ae8e8a76c6ea9399b3f5
jjosephd <jjosephd804@gmail.com>
2025-01-09 18:57:43 -0500

    Hero section updated

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,6 +4,7 @@ import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './header/header.component';
 import { NavComponent } from './nav/nav.component';
 import { HeroComponent } from './hero/hero.component';
+import { MainInputComponent } from './main-input/main-input.component';
 import { DealsComponent } from './deals/deals.component';
 import { DetailsComponent } from './details/details.component';
 import { RouterModule } from '@angular/router';
@@ -13,6 +14,7 @@ import { RouterModule } from '@angular/router';
   imports: [
     HeaderComponent,
     NavComponent,
+    MainInputComponent,
     HeroComponent,
     DealsComponent,
     DetailsComponent,
---
+++ src/app/main-input/main-input.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-main-input',
+  imports: [],
+  templateUrl: './main-input.component.html',
+  styleUrl: './main-input.component.css'
+})
+export class MainInputComponent {
+
+}
