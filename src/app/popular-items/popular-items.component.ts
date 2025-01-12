72393d569853284358493ab767b7f38161bc655c
jjosephd <jjosephd804@gmail.com>
2025-01-12 12:07:33 -0500

    Items added to popular items section. Scroll container created for popular items.

--- src/app/popular-items/popular-items.component.ts
+++ src/app/popular-items/popular-items.component.ts
@@ -1,11 +1,24 @@
 import { Component } from '@angular/core';
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { LazyLoadImageModule } from 'ng-lazyload-image';
 
+interface popularItems {
+  popularItem: string;
+}
 @Component({
   selector: 'app-popular-items',
-  imports: [],
+  imports: [CommonModule, LazyLoadImageModule],
   templateUrl: './popular-items.component.html',
-  styleUrl: './popular-items.component.css'
+  styleUrl: './popular-items.component.css',
 })
 export class PopularItemsComponent {
-
+  popularItems: popularItems[] = [
+    { popularItem: '../assets/deals/popularItem_1.jpg' },
+    { popularItem: '../assets/deals/popularItem_2.jpg' },
+    { popularItem: '../assets/deals/popularItem_3.jpg' },
+    { popularItem: '../assets/deals/popularItem_4.jpg' },
+    { popularItem: '../assets/deals/popularItem_5.jpg' },
+    { popularItem: '../assets/deals/popularItem_6.jpg' },
+  ];
 }
